d4dfe251071488ff71e9147c51d195b7d5b781af
Mary Dault <bluestar1dault@gmail.com>
2023-06-17 19:47:52 -0400

    organize files

--- routes/api/thoughtRoutes.js
+++ routes/api/thoughtRoutes.js
@@ -10,12 +10,23 @@ const {
     deleteReaction
 } = require('../../controllers/thought-controller');
 
-router.route('/').get(getThoughts).post(createThought);
+router
+    .route('/')
+    .get(getThoughts)
+    .post(createThought);
 
-router.route('/:thoughtId').get(getSingleThought).put(updateThought).delete(deleteThought);
+router
+    .route('/:thoughtId')
+    .get(getSingleThought)
+    .put(updateThought)
+    .delete(deleteThought);
 
-router.route('/:thoughtId/reactions').post(addReaction);
+router
+    .route('/:thoughtId/reactions')
+    .post(addReaction);
 
-router.route('/:thoughtId/reactions/:reactionId').delete(deleteReaction);
+router
+    .route('/:thoughtId/reactions/:reactionId')
+    .delete(deleteReaction);
 
 module.exports = router;
